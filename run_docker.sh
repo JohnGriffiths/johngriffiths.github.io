98d8f1df0c72f5c2ec6c111258ba587c1a4327c6
John Griffiths <JohnGriffiths@users.noreply.github.com>
2018-11-14 10:35:21 -0500

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #sudo docker build -t pelican .
-docker pull rossant/pelican
-docker run -t --name=pelican-run -v $(pwd):/site rossant/pelican
+docker pull johngriffiths/pelican
+docker run -t --name=pelican-run -v $(pwd):/site johngriffiths/pelican
 docker rm pelican-run
